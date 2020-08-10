f401e70321d2546020634ca0285bff4582a27116
Kalpit Muley <kalpitmuley0@gmail.com>
2020-08-10 12:05:07 +0530

    Added comments

--- src/main/java/study/assignment2/Project1/service/BookService.java
+++ src/main/java/study/assignment2/Project1/service/BookService.java
@@ -7,13 +7,39 @@ import java.util.List;
 
 public interface BookService {
 
+    /**
+     * @return All the books stored in the DB
+     */
     List<BookDto> getAllBooks();
 
+    /**
+     * Add a book to the Database
+     *
+     * @param bookDto Book to be added
+     */
     void addBook(BookDto bookDto);
 
+    /**
+     * Add List of books in the Database
+     *
+     * @param bookDtos List of books
+     */
     void addMultipleBooks(List<BookDto> bookDtos);
 
+    /**
+     * Update the AuthorName to updatedName if the the first name uthor name is prevoiusName
+     *
+     * @param prevoiusName First name of the Author
+     * @param updatedName  New name of the Author
+     * @return Number of entries modified
+     */
     Integer updateAuthorName(String prevoiusName, String updatedName);
 
+    /**
+     * Delete the Books whose cost is more than @param cost
+     *
+     * @param cost Cost of the book
+     * @return Number of entries deleted
+     */
     Integer deleteBooksCostlierThan(Integer cost);
 }
