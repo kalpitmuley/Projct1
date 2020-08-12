5995e2b798ddc8a63e62a71ffd00328ca69a048a
Kalpit Muley <kalpitmuley0@gmail.com>
2020-08-12 19:38:08 +0530

    Added comments

--- src/main/java/study/assignment2/Project1/controller/BookResource.java
+++ src/main/java/study/assignment2/Project1/controller/BookResource.java
@@ -20,15 +20,24 @@ public class BookResource {
     @Autowired
     BookService bookService;
 
+    /**
+     * Getting the port value from property file
+     */
     @Value("${server.port}")
     private String serverPort;
 
+    /**
+     * @return Port on which server is running
+     */
     @GetMapping("/server/port")
     public String getServerPort() {
         logger.info("Server port API returned port: " + serverPort);
         return "Server is running on port: " + serverPort;
     }
 
+    /**
+     * @return List of all the books in the DB
+     */
     @GetMapping("/book/all")
     public List<BookDto> getAllBooks() {
         List<BookDto> bookList = bookService.getAllBooks();
@@ -36,6 +45,11 @@ public class BookResource {
         return bookList;
     }
 
+    /**
+     * API to add a book to the DB
+     *
+     * @param bookDto Book to be added
+     */
     @PostMapping("/book/add")
     @ResponseStatus(HttpStatus.CREATED)
     public void addBook(@RequestBody BookDto bookDto) {
@@ -43,6 +57,12 @@ public class BookResource {
         bookService.addBook(bookDto);
     }
 
+    /**
+     * API to add multiple books to the DB
+     *
+     * @param bookDtos List of books to be added
+     * @return number of books added to the DB
+     */
     @PostMapping("/book/addMultiple")
     @ResponseStatus(HttpStatus.CREATED)
     public String addMultipleBooks(@RequestBody List<BookDto> bookDtos) {
@@ -51,21 +71,34 @@ public class BookResource {
         return bookDtos.size() + " Books Added to the DB";
     }
 
+    /**
+     * API to update the author name
+     *
+     * @param previousName Previous first name of the Author. Default value is Will
+     * @param updatedName  Updated name of the Author. Default value is Williams
+     * @return Number of Rows updated
+     */
     @PutMapping("/book/updateAuthor")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public String updateAuthorName(@RequestParam(value = "previousName", required = false, defaultValue = "Will") String previousName,
-                                 @RequestParam(value = "newName", required = false, defaultValue = "Williams") String updatedName) {
+                                   @RequestParam(value = "newName", required = false, defaultValue = "Williams") String updatedName) {
         logger.info("Updating the Author name from" + previousName + " to " + updatedName);
         Integer updatedRows = bookService.updateAuthorName(previousName, updatedName);
-        return updatedRows +" Rows updated";
+        return updatedRows + " Rows updated";
     }
 
+    /**
+     * API to delete books which ccost more than given cost.
+     *
+     * @param cost cost of the book which needs to be deleted. Default value is 1000
+     * @return Number of entries deleted
+     */
     @DeleteMapping("/book/delete")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public String deleteBooksCostlierThan(@RequestParam(value = "cost", required = false, defaultValue = "1000") Integer cost) {
         logger.info("Deleting books costlier than: " + cost);
         Integer deletedRows = bookService.deleteBooksCostlierThan(cost);
-        return deletedRows +" entries deleted";
+        return deletedRows + " entries deleted";
     }
 
 }
